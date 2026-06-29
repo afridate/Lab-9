5c9156b8b3d7e17aa4a4befe80fd4980b0c318c5
afridate <mikdarja9@gmail.com>
2026-06-29 09:38:32 +0300

    main.py

--- main.py
+++ main.py
@@ -1,72 +1,48 @@
+"""
+Лаб9
+Вариант 4 -колво шагов с суммой
+(поля ввода: steps,date)
+Афрузунова Дарья 502744
+"""
+
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
-
-app = Flask('Furniture store')
+app = Flask('Steps tracker')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 
-# products = [
-#     {'prod_name': 'sofa',
-#      'price': 12000,
-#      'in_stock': False,
-#      'id': 0},
-#     {'prod_name': 'table',
-#      'price': 6000,
-#      'in_stock': True,
-#      'id': 1},
-#     {'prod_name': 'chair',
-#      'price': 8000,
-#      'in_stock': False,
-#      'id': 2},
-# ]
-
-
-class Product(db.Model):
+class StepsRecord(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    prod_name = db.Column(db.String(300))
-    price = db.Column(db.Integer)
-    in_stock = db.Column(db.Boolean, default=True)
+    steps = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
-        return f'Product{self.id}. {self.prod_name} - {self.price} rub.'
+        return f'StepsRecord{self.id}. {self.date} - {self.steps} шагов'
 
 
 @app.route('/')
 def main():
-    products = Product.query.all()
-    return render_template('index.html', products_list=products)
-
-
-@app.route('/in_stock/<product_id>', methods=['PATCH'])
-def modify_product(product_id):
-    product = Product.query.get(product_id)
-    product.in_stock = request.json['in_stock']
-    db.session.commit()
-    # global products
-    # in_stock = request.json['in_stock']
-    # for product in products:
-    #     if product['id'] == product_id:
-    #         product.update({'in_stock': in_stock})
-    # return 'OK'
+    records = StepsRecord.query.all()
+    total_steps = sum(record.steps for record in records)
+    return render_template('index.html', records=records, total_steps=total_steps)
 
 
 @app.route('/add', methods=['POST'])
-def add_product():
+def add_record():
     data = request.json
-    product = Product(**data)
-    db.session.add(product)
+    record = StepsRecord(steps=data['steps'], date=data['date'])
+    db.session.add(record)
     db.session.commit()
-
-    # id_last = products[-1]['id']
-    # id_new = id_last + 1
-    # data['id'] = id_new
-    # products.append(data)
     return 'OK'
 
+
+@app.route('/clear', methods=['POST'])
+def clear_records():
+    StepsRecord.query.delete()
+    db.session.commit()
+    return 'OK'
 
 
 if __name__ == '__main__':
